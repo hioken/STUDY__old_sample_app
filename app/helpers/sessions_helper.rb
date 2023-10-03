acb5cc85d18793c3e03e9e6cd30b4b9c569db96a
hioken <radcloseff@gmail.com>
2023-10-03 17:08:50 +0000

    9.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
-  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    session[:session_token] = user.session_token
   end
   
   def remember(user)
@@ -12,7 +12,10 @@ module SessionsHelper
   
   def current_user
     if user_id = session[:user_id]
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.remember_digest
+        @current_user = user
+      end
     elsif user_id = cookies.encrypted[:user_id]
       user = User.find_by(id: user_id)
       if user&.authenticated?(cookies[:remember_token])
