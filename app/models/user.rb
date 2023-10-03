2806b14702bff9373e4c9426736512d3b5d0ff5b
hioken <radcloseff@gmail.com>
2023-10-03 17:45:40 +0000

    10.2.3-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,10 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
   has_secure_password
   
   def self.digest(string)
